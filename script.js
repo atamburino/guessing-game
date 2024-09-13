80b5bb62f13e30d492e9a880258d39069b612499
Andy Tamburino <andytamburino@gmail.com>
2024-09-13 15:32:33 -0400

    notes to solve task + proof of life

---
+++ script.js
@@ -0,0 +1,48 @@
+// Variables
+let Life = 'I am alive'; // Proof of life
+
+// Notes:
+// Leverage the Math.random() function to generate a random number between 1 and 50
+
+
+
+
+
+// Give the user a prompt to enter their name - We want to use the window prompt
+// to ask the user their number
+
+
+
+
+
+// We want to track the user's number of guesses
+
+
+
+
+
+// Use if/else and truthy/falsy concepts to evaluate whether or not the user's guess was correct.
+// NOTE: If a user's guess is incorrect, respond with a prompt letting them know whether or not their guess was too high or too low and how many chances remain.
+
+
+
+
+
+// 5. If a user guesses within three chances, display an alert letting the user know they've won and what the number was.
+
+
+
+
+
+//6. If a user loses, display an alert letting the user know they have lost and reveal the number.
+
+
+
+
+
+// other goals -- 
+// - Return an error message if the user does NOT enter a guess that is a number. Do NOT count that guess as a valid attempt. Let the user know how many guesses remain.
+// - Provide a user with the option to restart the game WITHOUT using a loop. This is tricky! Only do this step if you are absolutely finished with all the other requirements and want to push yourself.
+
+
+console.log(Life);
